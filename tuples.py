5a50164305f38d073f12a0075c83335bdba55e18
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-10 15:14:00 -0700

    intraduction to tuples

---
+++ tuples.py
@@ -0,0 +1,13 @@
+mytuple =(1,2,3,4,5 ,)
+print(mytuple)
+item =mytuple[1:4]
+print(item)
+
+if 3 in mytuple :
+    print('yes')
+else:
+    print('no')
+
+print(len(mytuple))
+print(mytuple.count(16))
+print(mytuple.index(3))

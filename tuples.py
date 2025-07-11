affb64520e57dbffd461626ba7b2be18902c812f
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-11 01:43:54 -0700

    tuples method

--- tuples.py
+++ tuples.py
@@ -11,3 +11,16 @@ else:
 print(len(mytuple))
 print(mytuple.count(16))
 print(mytuple.index(3))
+
+my_tuple =("Blaise","pascal","kigali")
+name,lastname,cite = my_tuple
+print(name)
+print(lastname)
+print(cite)
+
+import sys
+
+my_list =[1,2,3,"hello Guys",4,5]
+my_tuple =(1,2,3,"hello Guys",4,5)
+print(sys.getsizeof(my_list), "bytes")
+print(sys.getsizeof(my_tuple), "bytes")

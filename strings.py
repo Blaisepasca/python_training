fe27abb8600eedb286bf765938203db096782ab4
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-11 14:42:36 -0700

    string

--- strings.py
+++ strings.py
@@ -6,11 +6,32 @@ my_string =my_string.replace('wordld','world')
 my_string =my_string.upper()
 print(my_string)
 
-my_string = my_string.endswith('world')
-print(my_string)
-my_string = my_string.start('hello')
-print(my_string)
-my_string = my_string.find('world')
-print(my_string)
-my_string = my_string.split("")
-print(my_string)
+# my_string = my_string.endswith('world')
+# print(my_string)
+# my_string = my_string.start('hello')
+# print(my_string)
+# my_string = my_string.find('world')
+# print(my_string)
+# my_string = my_string.split("")
+# print(my_string)
+
+Greating = 'hello, everyone, here, hope, you, are, well'
+print(Greating)
+my_greating = Greating.split(",")
+print(my_greating)
+New_greating =' '.join(my_greating)
+print(New_greating)
+
+
+
+#othermethods 
+# find
+# count
+# startswith
+# slpit
+# join
+# lower
+# capitalize
+# title
+# isalpha
+# isalmun

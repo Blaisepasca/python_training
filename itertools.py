0a5e6a1af5d837d073b18af10ff78df206ddea4b
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-12 08:06:36 -0700

    itertools methodes

--- itertools.py
+++ itertools.py
@@ -1,7 +1,36 @@
-from itertools import product
+# from itertools import product
 
-a =[1,2]
-b=[3,4]
-prod =product(a,b, repeat =2)
-print(list(prod))
+# a =[1,2]
+# b=[3,4]
+# prod =product(a,b, repeat =2)
+# print(list(prod))
+
+
+# from itertools import permutations
+
+# a =[1,2,3]
+# perm =permutations(a)
+# print(list(perm))
+
+
+# f
+
+from itertools import groupby
+
+
+persons =[{name :'Paci','age':35},{'name':'Aimerance','age':17},
+          {'name':'Leah','age':25},{'name':'koko','age':35}]
+
+goup_obj =groupby(persons, key =lambda x: x['age'])
+for key, value in group_obj:
+    print(key,list(value))
+
+# from itertools import operator
+# from itertools import count,cycle,repeat
+
+
+
+
+
+
 

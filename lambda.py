0481c1fea740c8894fd899589518c2a11579a539
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-12 15:37:30 -0700

    lambda  methodes

---
+++ lambda.py
@@ -0,0 +1,13 @@
+#lambda agruments : expression
+add10 = lambda x: x + 10
+# Example usage
+print(add10(5))  # Output: 15
+print(add10(20))  # Output: 30
+
+def add10_func(x):
+    return x + 10
+
+mult = lambda x,y: x+y
+# Example usage
+print(mult(5, 10))  # Output: 15
+print(mult(20, 30))  # Output: 50

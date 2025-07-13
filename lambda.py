fa9b73a8382eb7f09b531eb91363535a88787159
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-13 07:07:06 -0700

    lambda examples

--- lambda.py
+++ lambda.py
@@ -17,3 +17,18 @@ points2D_sorted = sorted(points2D, key=lambda x: x[1])
 
 print(points2D_sorted)  # Output: [(10, -4), (1, 2), (3, 4), (5, 6)]    
 print(points2D_sorted)
+
+
+#example
+a =[1, 2, 3, 4, 5]
+b =map(lambda x: x+2, a)
+print(list(b))
+
+c =[x*2 for x in a]
+print(c)
+
+d =[x/1 for x in a]
+print(d)
+
+e =[x-1 for x in a]
+print(e)

27ccb0f56170e256459bdfb1b125b43719499cf6
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-15 01:15:45 -0700

    random

--- random_example.py
+++ random_example.py
@@ -3,3 +3,22 @@ import random
 mylist = list("QBERCYIL")
 a = random.choice(mylist)
 print(a)
+
+mylist = list("QBERCYIL")
+a = random.sample(mylist ,3)
+print(a)
+
+mylist = list("QBERCYIL")
+a = random.choice(mylist)
+print(a)
+
+mylist = list("QBERCYIL")
+a = random.shuffle(mylist)
+print(mylist)
+
+#other examples
+
+import numpy as np
+
+a = np.random.randint(0, 10, (3,4))
+print(a)

9257c1ebe094770867dbedda83ec1324b14f4a7c
Blaise pascal <blaiseaganzepascal10@gmail.com>
2025-07-15 06:18:12 -0700

    functions

--- funcs.py
+++ funcs.py
@@ -1,23 +1,45 @@
-def foo(a,b,c,d=9):
-    print(a, b, c,d)
-
-foo(c=1, b=2, a=3)
-
-
-
-
-
+def foo():
+    global number
+    number = 45
+   
+number =0
+foo()
+print(number)
 
 
-
-
+person =['blaise'] *5
+print(person)
 
 
 
 
 
+# def foo(a,b,c,d=9):
+#     print(a, b, c,d)
 
-def print_name(name):
-    print(name)
+# foo(c=1, b=2, a=3)
 
-print_name("Blaise")
+# def foo(*args,last):
+#     for arg in args:
+#         print(last)
+#         print(arg)
+
+# foo(1, 2, 3, last=100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def print_name(name):
+#     print(name)
+
+# print_name("Blaise")
--- threading_example.py
+++ threading_example.py
@@ -1,46 +1,47 @@
-from threading import Thread
-import time
+# from threading import Thread
+# import time
 
-# database_value = 0
+# # database_value = 0
 
-# def increase():
-#     global database_value
-#     # copier la valeur actuelle dans une variable locale
-#     local_copy = database_value
-#     local_copy += 1
-#     time.sleep(0.1)
-#     database_value = local_copy
+# # def increase():
+# #     global database_value
+# #     # copier la valeur actuelle dans une variable locale
+# #     local_copy = database_value
+# #     local_copy += 1
+# #     time.sleep(0.1)
+# #     database_value = local_copy
 
+# # if __name__ == "__main__":
+
+# #     print('start value:', database_value)
+
+# #     Thread1 = Thread(target=increase)
+# #     Thread2 = Thread(target=increase)
+
+# #     Thread1.start()
+# #     Thread2.start()
+
+# #     Thread1.join()
+# #     Thread2.join()
+
+# #     print('end value:', database_value)
+
+# def add_100(number):
+#     print('start add 100')
+#     time.sleep(0.01)
+#     number.value += 1
 # if __name__ == "__main__":
+#     shared_number = value('i', 0)
+#     print('number at beginning:', shared_number.value)
 
-#     print('start value:', database_value)
+# p1 = process(target=add_100, args=(shared_number,))
+# p2 = process(target=add_100, args=(shared_number,))
 
-#     Thread1 = Thread(target=increase)
-#     Thread2 = Thread(target=increase)
+# p1.start()
+# p2.start()
 
-#     Thread1.start()
-#     Thread2.start()
+# p1.join()
+# p2.join()
 
-#     Thread1.join()
-#     Thread2.join()
+# print('number at end:', shared_number.value)
 
-#     print('end value:', database_value)
-from threading import Thread
-import time
-
-def count():
-    for i in range(5):
-        print(f"Thread counting: {i}")
-        time.sleep(1)
-
-if __name__ == "__main__":
-    thread1 = Thread(target=count)
-    thread2 = Thread(target=count)
-
-    thread1.start()
-    thread2.start()
-
-    thread1.join()
-    thread2.join()
-
-    print("Fin du programme.")
